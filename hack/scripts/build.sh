f1486e3636c79bbf0a6a9447868b301eb977594a
Zach Dunton <fenfir@gmail.com>
2022-02-02 19:54:06 +0100

    Add build for raspberry pi

--- hack/scripts/build.sh
+++ hack/scripts/build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 cd /go/src/github.com/everadaptive/mindlights
 
+rm -rf build
+
 ## linux
 apt update
 apt install -y libusb-1.0-0-dev
 
-mkdir -p build/linux/
-GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go build -o build/linux/mindlights .
+mkdir -p build/linux_amd64/
+GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go build -o build/linux_amd64/mindlights .
+
+mkdir -p build/linux_arm7/
+dpkg --add-architecture armhf
+apt update
+apt install -y crossbuild-essential-armhf libusb-1.0-0-dev:armhf
+GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=1 CC=arm-linux-gnueabihf-gcc  go build -o build/linux_arm7/mindlights .
 
 ## macOS
 curl -L -H "Authorization: Bearer QQ==" -o /tmp/libusb.tar.gz https://ghcr.io/v2/homebrew/core/libusb/blobs/sha256:e202da5a53b0955b4310805b09e9f4af3b73eed57de5ae0d44063e84dca5eafd 
